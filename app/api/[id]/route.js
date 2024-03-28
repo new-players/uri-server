0954f0b5fd54052ca8d7c89cde282417894a175e
Paul Gadi <paulgadi@gmail.com>
2024-03-28 14:11:55 -0400

    add base metadata

--- app/api/[id]/route.js
+++ app/api/[id]/route.js
@@ -1,12 +1,11 @@
-import { promises as fs } from 'fs'; 
+import jsonObject from "/app/data.json" assert { type: "json" };
 
 export async function GET(request, { params }) {
-  const file = await fs.readFile(process.cwd() + '/app/data.json', 'utf8');
-  const data = JSON.parse(file);
 
-  return new Response(`${data.name} ${data.description}`);
+  const metadata = 
+    jsonObject.base_metadata
 
+    metadata.name = metadata.name + " #" + params.id
 
-  // // we will use params to access the data passed to the dynamic route
-    // return new Response(`Welcome to my Next application, user: ${params.id}`);
+  return new Response(JSON.stringify(metadata));
   }
