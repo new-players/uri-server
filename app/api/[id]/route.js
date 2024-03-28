1a45b1822f1bfed2d01c744d18fa9a08ffc98d7c
Paul Gadi <paulgadi@gmail.com>
2024-03-28 13:34:28 -0400

    load data.json

--- app/api/[id]/route.js
+++ app/api/[id]/route.js
@@ -1,4 +1,12 @@
+import { promises as fs } from 'fs'; 
+
 export async function GET(request, { params }) {
-    // we will use params to access the data passed to the dynamic route
-    return new Response(`Welcome to my Next application, user: ${params.id}`);
+  const file = await fs.readFile(process.cwd() + '/app/data.json', 'utf8');
+  const data = JSON.parse(file);
+
+  return new Response(`${data.name} ${data.description}`);
+
+
+  // // we will use params to access the data passed to the dynamic route
+    // return new Response(`Welcome to my Next application, user: ${params.id}`);
   }
