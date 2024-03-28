c87975601538880035e678f1baf4e8966ea7c676
Paul Gadi <paulgadi@gmail.com>
2024-03-28 14:37:43 -0400

    add animation route

--- app/api/[id]/route.js
+++ app/api/[id]/route.js
@@ -5,6 +5,8 @@ export async function GET(request, { params }) {
   let metadata = jsonObject.base_metadata
 
   metadata.name = jsonObject.name + " #" + params.id
+  metadata.image = jsonObject.base_metadata.image + "/" + params.id
+  metadata.animation_url = jsonObject.base_metadata.animation_url + "/" + params.id
 
   return new Response(JSON.stringify(metadata));
   }
