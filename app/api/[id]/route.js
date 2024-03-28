a596262363d2fbd179bbb8f7dd37bd193d72846d
Paul Gadi <paulgadi@gmail.com>
2024-03-28 15:57:40 -0400

    Update route.js

--- app/api/[id]/route.js
+++ app/api/[id]/route.js
@@ -2,7 +2,7 @@ import jsonObject from "/app/data.json" assert { type: "json" };
 
 export async function GET(request, { params }) {
 
-  let metadata = jsonObject.base_metadata
+  let metadata = {...jsonObject.base_metadata}
 
   metadata.name = jsonObject.name + " #" + params.id
   metadata.image = jsonObject.base_metadata.image + "/" + params.id
