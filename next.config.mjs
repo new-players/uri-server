9ea024c68d8bbe9abfdb397cc5726af330342521
jaeson-outplay <jaeson@outplay.games>
2024-04-17 13:35:55 +0800

    temporary cors for devt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // ... other Next.js configurations (if any)
+  
+    async headers() => [
+      {
+        source: '*',  // Allow requests from any origin (localhost for development)
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: 'http://localhost:3000' },
+          { key: 'Access-Control-Allow-Methods', value: '*' }, // Allow all methods for development
+          { key: 'Access-Control-Allow-Headers', value: '*' }, // Allow all headers for development
+        ],
+      },
+    ],
+  };
+  
+  export default nextConfig;
