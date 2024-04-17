654ef5cbcb58ed8e0cb36f2cd32bdea266fa2710
jaeson-outplay <jaeson@outplay.games>
2024-04-17 13:40:28 +0800

    fixing build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // ... other Next.js configurations (if any)
-  
-    async headers() => [
-      {
-        source: '*',  // Allow requests from any origin (localhost for development)
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: 'http://localhost:3000' },
-          { key: 'Access-Control-Allow-Methods', value: '*' }, // Allow all methods for development
-          { key: 'Access-Control-Allow-Headers', value: '*' }, // Allow all headers for development
-        ],
-      },
-    ],
-  };
-  
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }
+}
   export default nextConfig;
